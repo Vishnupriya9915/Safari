ea03868cb9bbc63612d251117a078a9dbb11f8ad
Vishnupriya <vishnupriya9915@gmail.com>
2020-06-03 14:22:31 +0530

    Completed build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
+
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -8,7 +14,79 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-module.exports= {
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+        test: /\.css$/i,
+        use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins:postCSSPlugins}}]
+    }
+
+let config = {
+    entry: './app/assets/scripts/App.js',
+    plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+}
+if(currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader')
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    }
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname,'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    }
+    config.mode = 'development'
+}
+if(currentTask == 'build') {
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    postCSSPlugins.push(require('cssnano'))
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    }
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+        )
+}
+
+
+/* initially was used */
+let deleteMeLater = {
     entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
@@ -33,3 +111,5 @@ module.exports= {
         ]
     }
 }
+
+module.exports = config
